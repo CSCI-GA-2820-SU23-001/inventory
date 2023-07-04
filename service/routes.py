8fecfdbcfc103bb3fcc3b9c63fcfd804e57262cb
django9847 <ygp2005@nyu.edu>
2023-07-04 02:12:02 +0000

    Initial commit

--- service/routes.py
+++ service/routes.py
@@ -29,3 +29,24 @@ def index():
 ######################################################################
 
 # Place your REST API code here ...
+
+######################################################################
+# DELETE A RECOMMENDATION
+######################################################################
+
+@app.route("/inventory/<int:id>", methods=["DELETE"])
+def delete_inventory(id):
+    app.logger.info("Request to delete inventory with ID %s", id)
+    product_id = Inventory.find(id)
+    if product_id:
+        condition = product_id.condition
+        if condition == Condition.NEW:
+            product_id.delete(id)
+            app.logger.info("Inventory with ID %s and condition %s deleted successfully.", id, condition)
+            return "", status.HTTP_204_NO_CONTENT
+        else:
+            app.logger.info("Cannot delete inventory with ID %s and condition %s.", id, condition)
+            return "Cannot delete inventory with the specified condition.", status.HTTP_403_FORBIDDEN
+    else:
+        app.logger.info("Inventory with ID %s not found.", id)
+        return "", status.HTTP_404_NOT_FOUND
--- tests/test_routes.py
+++ tests/test_routes.py
@@ -10,7 +10,7 @@ import logging
 from unittest import TestCase
 from unittest.mock import MagicMock, patch
 from service import app
-from service.models import db
+from service.models import Inventory, db
 from service.common import status  # HTTP Status Codes
 
 
@@ -34,7 +34,6 @@ class TestYourResourceServer(TestCase):
 
     def tearDown(self):
         """ This runs after each test """
-
     ######################################################################
     #  P L A C E   T E S T   C A S E S   H E R E
     ######################################################################
@@ -43,3 +42,13 @@ class TestYourResourceServer(TestCase):
         """ It should call the home page """
         resp = self.client.get("/")
         self.assertEqual(resp.status_code, status.HTTP_200_OK)
+
+    def test_delete_inventory(self):
+        """It should Delete a Inventory"""
+        test_inventory=Inventory(product_id=1, condition=Condition.NEW.name, quantity=10, restock_level=1)
+        response = self.client.delete(f"{BASE_URL}/{test_inventory.id}")
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+        self.assertEqual(len(response.data), 0)
+        # makes sure they are deleted
+        response = self.client.get(f"{BASE_URL}/{test_inventory.id}")
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
